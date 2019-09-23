16174f642c214852c9e7151b97f0e3a008f0555b
AndersonFSP <anfisope10@hotmail.com>
2019-09-23 18:09:42 -0300

    Foi adicionado a opção planeta na busca por informações

--- StarWarsAPI/script.js
+++ StarWarsAPI/script.js
@@ -1,36 +1,63 @@
 let icon = window.document.getElementById('icon')
 function buscar(){
+    name.innerHTML = ''
+    height.innerHTML = ''
+    mass.innerHTML = ''
+    gender.innerHTML = ''
+    planet.innerHTML = ''
+    let sel = document.getElementById('selopt').value
     let id = document.getElementById('num').value;
-    let api = `https://swapi.co/api/people/${id}/?format=json`
+    let api = `https://swapi.co/api/${sel}/${id}/?format=json`
     let request = new XMLHttpRequest();
     request.open('GET', api);
-    request.onload = function(){
-        let info = JSON.parse(request.responseText);
 
-        let name = document.getElementById('name')
-        name.innerHTML =  `<h3 style='color:yellow;'> NOME:</h3>  ${info.name}` 
-        icon.src = `image/${info.name}.jpg`
+    if (sel=='people'){
+        request.onload = function(){
+            let info = JSON.parse(request.responseText);
 
-        let height = document.getElementById('height')
-        height.innerHTML = `<h3 style='color:yellow;'> ALTURA:</h3> ${info.height}`
+            let name = document.getElementById('name')
+            name.innerHTML =  `<h3 style='color:yellow;'> NOME:</h3>  ${info.name}` 
+            icon.src = `image/${info.name}.jpg`
 
-        let mass = document.getElementById('mass')
-        mass.innerHTML = `<h3 style='color:yellow;'> MASSA:</h3> ${info.mass}`
+            let height = document.getElementById('height')
+            height.innerHTML = `<h3 style='color:yellow;'> ALTURA:</h3> ${info.height}`
 
-        let gender = document.getElementById('gender')
-        gender.innerHTML = `<h3 style='color:yellow;'> GÊNERO:</h3> ${info.gender}`
+            let mass = document.getElementById('mass')
+            mass.innerHTML = `<h3 style='color:yellow;'> MASSA:</h3> ${info.mass}`
 
-            let api2 = `https://swapi.co/api/planets/${id}/?format=json`
-            let request2 = new XMLHttpRequest();
-            request2.open('GET', api2);
-            request2.onload = function(){
-                let planetinfo = JSON.parse(request2.responseText);
+            let gender = document.getElementById('gender')
+            gender.innerHTML = `<h3 style='color:yellow;'> GÊNERO:</h3> ${info.gender}`
 
-                let planet = document.getElementById('planet');
-                planet.innerHTML = `<h3 style='color:yellow;'> PLANETA NATAL:</h3> ${planetinfo.name}`
+                let api2 = `https://swapi.co/api/planets/${id}/?format=json`
+                let request2 = new XMLHttpRequest();
+                request2.open('GET', api2);
+                request2.onload = function(){
+                    let planetinfo = JSON.parse(request2.responseText);
 
-                }
-        request2.send()
+                    let planet = document.getElementById('planet');
+                    planet.innerHTML = `<h3 style='color:yellow;'> PLANETA NATAL:</h3> ${planetinfo.name}`
+
+                    }
+            request2.send()
+            }
+        request.send()
+    }else{
+        request.onload = function(){
+            let info = JSON.parse(request.responseText);
+            
+            let name = document.getElementById('name')
+            name.innerHTML = `<h3 style='color:yellow;'> NOME:</h3>  ${info.name}`
+            icon.src = `image/${info.name}.jpg`
+
+            let height = document.getElementById('height')
+            height.innerHTML = `<h3 style='color:yellow;'> Periodo de Rotação:</h3> ${info.rotation_period}`
+
+            let mass = document.getElementById('mass')
+            mass.innerHTML = `<h3 style='color:yellow;'> Periodo Orbital:</h3> ${info.orbital_period}`
+
+            let gender = document.getElementById('gender')
+            gender.innerHTML = `<h3 style='color:yellow;'> Diametro:</h3> ${info.diameter}`
         }
-    request.send()
+        request.send()
+    }
 }
